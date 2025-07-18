547283ed6809add673ceb1531b10e3511043d220
Cedric <cedricctf11a@gmail.com>
2025-07-18 18:04:27 +0800

    added chatbot component to entire app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import "@/styles/globals.css";
+import Chatbot from '../components/Chatbot';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Chatbot />
+    </>
+  );
 }
+
+export default MyApp;
